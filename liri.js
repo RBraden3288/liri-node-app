97fe99d40830f2ba526f38d566f9e4c0e9ed8032
Randall Braden <randalllb@Randalls-MBP-2.attlocal.net>
2019-05-19 12:18:05 -0700

    finished product without inquirer

--- liri.js
+++ liri.js
@@ -1,41 +1,82 @@
-//add code to read and set any environment variables with the dotenv package//
-require("dotenv").config();3
 
+//add code to read and set any environment variables with the dotenv package//
+require("dotenv").config();
 //import the `keys.js` file and store it in a variable//
 var keys = require("./key.js");
+var fs = require("fs")
+var moment = require("moment");
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var divider = "\n**=**=**=**=**=**=**=**=**=**=**=**\n"
 
-                        //BANDS IN TOWN//
+//===BANDS IN TOWN===//
+//command: `concert-this`//
+var artist = process.argv[2]
+var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
+  axios.get(queryURL).then(
+    function(response){
+      var jsonData = response.data[1];
+      var data = [
+        "Artist Lineup: " + jsonData.lineup,
+        "Venue Name: " + jsonData.venue.name,
+        "Date of Event: " + moment(jsonData.datetime).format("l"),
+        ].join("\n");
+        console.log(divider + "\nHere is information on the next event: \n" + data + "\n\n\n")
+    }
+)
 
-                        // === SPOTIFY API === //
+// === SPOTIFY API === //
+//command: `spotify-this-song`//
 
-// var Spotify = require("node-spotify-api");
-// var spotify = new Spotify(keys.spotify);
-// var request = process.argv[2];
+var song = process.argv[3];
 
-// spotify
-//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-//   .then(function(data) {
-//     console.log(data); 
-//   })
-//   .catch(function(err) {
-//     console.error('Error occurred: ' + err); 
-//   });
+// spotify.search({ type: 'track', query: song }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+ 
+// console.log(data); 
+// });
 
+spotify
+  .search({ type: 'track', query: song })
+  .then(function(response) {
+    if (song == null) {
+      song = "the+sign"
+    } else {
+      var songResult = response.tracks.items[0]
+      console.log(divider, 
+        "\nYour search for " + song.toUpperCase() + " is loading...");
+        console.log(
+          "\nArtist(s): " + songResult.artists[0].name +
+          "\nSong Name: " + songResult.name +
+          "\nPreview Link: " + songResult.album.external_urls.spotify +
+          "\nAlbum: " + songResult.album.name 
+          )
+    }
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
-
-                        // === AXIOS FOR OMDB === //
-var axios = require("axios")
+// === AXIOS FOR OMDB === //
+//command: `movie-this`//
 var movieSearch = process.argv[4]
 
 axios.get("http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&apikey=trilogy").then(
   function(response) {
-      if (movieSearch == null){
-          movieSearch = "Mr. Nobody";
-          console.log("If you haven't watched Mr. Nobody, then you should: <http://www.imdb.com/title/tt0485947/>");
-          console.log("It's on Netflix!");
-      } else 
-      { console.log(
+    //console log Mr. Nobody if there is no input 
+    if (movieSearch == null){
+        movieSearch = "Mr. Nobody";
+        console.log("If you haven't watched Mr. Nobody, then you should: <http://www.imdb.com/title/tt0485947/>");
+        console.log("It's on Netflix!");  
+        } 
+    // console log response if movie input is valid
+    else { 
+        console.log(
+        divider,
         "\nTitle: " + response.data.Title + 
         "\nYear: " + response.data.Year +
         "\nIMDB Rating: " + response.data.imdbRating +
@@ -43,8 +84,11 @@ axios.get("http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&apikey=tr
         "\nCountry: " + response.data.Country +
         "\nLanguage: " + response.data.Language +
         "\nMovie Plot: " + response.data.Plot +
-        "\nActors: " + response.data.Actors
+        "\nActors: " + response.data.Actors + "\n\n\n"
         );
     }
   }
 );
+
+                // === DO WHAT IT SAYS === //
+                //command: `do-what-it-says`//
